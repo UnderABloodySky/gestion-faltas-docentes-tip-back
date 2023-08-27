dfdd07bf49c91dd00c641e622b81c559af391973
underabloodysky <valenzuelahoracioe@gmail.com>
2023-08-27 05:11:01 -0300

    Remove unnecesary annotation in TeacherNotFoundException

--- src/test/kotlin/ar/edu/unq/tpi/ciriaqui/exception/TeacherNotFoundExceptionTest.kt
+++ src/test/kotlin/ar/edu/unq/tpi/ciriaqui/exception/TeacherNotFoundExceptionTest.kt
@@ -5,10 +5,8 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest
 @ExtendWith(SpringExtension::class)
 class TeacherNotFoundExceptionTest {
     @Test
