4fc991f923fe71d9607bb6c33dff902ce45f4d8c
underabloodysky <valenzuelahoracioe@gmail.com>
2023-08-30 02:08:02 -0300

    Change in config.yml

---
+++ docker/data-test.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS TEACHERS (
+                                        id SERIAL PRIMARY KEY,
+                                        name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+    );
+
+INSERT INTO TEACHERS (name, email, password) VALUES ('teacher1', 'teacher1@unq.edu.ar', 'teacher1');
