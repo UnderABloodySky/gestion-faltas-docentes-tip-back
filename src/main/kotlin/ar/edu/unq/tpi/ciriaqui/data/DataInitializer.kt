c74f9c0a3d9f6b080a05d377e32cbc48f91a0017
underabloodysky <valenzuelahoracioe@gmail.com>
2023-09-09 04:03:54 -0300

    Add basic seeder with lacks

--- src/main/kotlin/ar/edu/unq/tpi/ciriaqui/data/DataInitializer.kt
+++ src/main/kotlin/ar/edu/unq/tpi/ciriaqui/data/DataInitializer.kt
@@ -1,6 +1,9 @@
 package ar.edu.unq.tpi.ciriaqui.data
 
+import ar.edu.unq.tpi.ciriaqui.dao.LackRepository
 import ar.edu.unq.tpi.ciriaqui.dao.TeacherRepository
+import ar.edu.unq.tpi.ciriaqui.model.Article
+import ar.edu.unq.tpi.ciriaqui.model.Lack
 import ar.edu.unq.tpi.ciriaqui.model.Teacher
 import org.hibernate.internal.util.collections.CollectionHelper.listOf
 import org.springframework.boot.ApplicationArguments
@@ -8,24 +11,29 @@ import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.time.LocalDate
 
 @Component
-class DataInitializer(val teacherRepository: TeacherRepository) : ApplicationRunner {
+class DataInitializer(val teacherRepository: TeacherRepository, val lackRepository: LackRepository) : ApplicationRunner {
 
     override fun run(args: ApplicationArguments) {
         val logger: Logger = LoggerFactory.getLogger(DataInitializer::class.java)
-
         logger.info("Seeder is running...")
-        logger.info(teacherRepository.count().toString())
-        if(teacherRepository.count()<= 0){
-            logger.info("Seeding")
-            val teacher0 = Teacher(name = "Albus Dumbledore", password = "asd123", email="teacher0@asd.cp")
-            val teacher1 = Teacher(name = "Minerva McGonagall", password = "asd123", email="teacher1@asd.cp")
-            val teacher2 = Teacher(name = "Remus Lupin", password = "asd123", email="teacher2@asd.cp")
-            val teacher3 = Teacher(name = "Sybill Trelawney", password = "asd123", email="teacher3@howgart")
-            val teacher4 = Teacher(name = "Quirinus Quirrell", password = "asd123", email="teacher3@howgart")
+        if(teacherRepository.count()<= 0) {
+            logger.info("Seeding TEACHERS")
+            val teacher0 = Teacher(name = "Albus Dumbledore", password = "asd123", email = "teacher0@asd.cp")
+            val teacher1 = Teacher(name = "Minerva McGonagall", password = "asd123", email = "teacher1@asd.cp")
+            val teacher2 = Teacher(name = "Remus Lupin", password = "asd123", email = "teacher2@asd.cp")
+            val teacher3 = Teacher(name = "Sybill Trelawney", password = "asd123", email = "teacher3@howgart")
+            val teacher4 = Teacher(name = "Quirinus Quirrell", password = "asd123", email = "teacher3@howgart")
             val teachers = listOf(teacher0, teacher1, teacher2, teacher3, teacher4)
             teacherRepository.saveAll(teachers)
+            logger.info("Seeding LACKS")
+            val lack0 = Lack(Article.PARTICULAR, LocalDate.of(2023, 12, 31),teacher2)
+            val lack1 = Lack(Article.STUDYDAY,LocalDate.of(2023, 12, 30),teacher4)
+            val lack2 = Lack(Article.STUDYDAY,LocalDate.of(2023, 12, 29),teacher4)
+            val lacks = listOf(lack0, lack1, lack2)
+            lackRepository.saveAll(lacks)
             logger.info("Seeding DONE")
         }
     }
